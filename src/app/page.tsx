dc6f7f41ae371fd47d5d70ee1dcc5c681b384c9e
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-12-09 12:28:42 +0500

    updated

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+ 
+    </div>
+  );
+}
